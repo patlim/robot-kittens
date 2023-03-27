5f275ac21147d8427f95f39bab6735dcf458dde5
Pat Lim <impatlim@gmail.com>
2023-03-27 14:13:12 +0200

    reverse last commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  imagesPublicPath: '/robot-kittens/_next/static/images/',
 }
 
 module.exports = nextConfig
