624d6ef0305fc14b00ab1463bb6a6a4d9f78e499
Pat Lim <impatlim@gmail.com>
2023-03-27 14:12:51 +0200

    next config image path change

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  imagesPublicPath: '/robot-kittens/_next/static/images/',
 }
 
 module.exports = nextConfig
